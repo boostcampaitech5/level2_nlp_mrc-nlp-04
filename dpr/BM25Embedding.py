4f42fdbb1b19d548e0ea444a3ac522012877855e
Yungi <dbsrlskfdk@naver.com>
2023-06-22 17:22:40 +0900

    [Feature] Add Example BM25 Rerank with DPR

--- dpr/BM25Embedding.py
+++ dpr/BM25Embedding.py
@@ -6,7 +6,7 @@ import pandas as pd
 from rank_bm25 import BM25Okapi
 from tqdm.notebook import tqdm
 
-def make_bm25_embedding(DATA_DIR, tokenizer, full_ds, context):
+def make_bm25_embedding(DATA_DIR, tokenizer, full_ds, context, test=False):
 	bm25 = BM25Okapi(context, tokenizer=tokenizer.tokenize)
 
 	train_iterator = tqdm(full_ds['question'], desc="BM25 Embedding")
@@ -17,7 +17,11 @@ def make_bm25_embedding(DATA_DIR, tokenizer, full_ds, context):
 
 	bm25_embedding = np.array(bm25_embedding)
 
-	with open(os.path.join(DATA_DIR, 'wiki_bm25_embedding.bin'), 'wb') as f:
-		pickle.dump(bm25_embedding, f)
+	if not test:
+		with open(os.path.join(DATA_DIR, 'wiki_bm25_embedding.bin'), 'wb') as f:
+			pickle.dump(bm25_embedding, f)
+	else:
+		with open(os.path.join(DATA_DIR, 'test_wiki_bm25_embedding.bin'), 'wb') as f:
+			pickle.dump(bm25_embedding, f)
 
 	return pd.DataFrame(bm25_embedding, index=full_ds['id'])
